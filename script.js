783f9c12611f550779697f21942a4f9762f4361c
uwamahoroange <uwamahoroange002@gmail.com>
2019-07-20 12:30:29 +0200

    add script

---
+++ script.js
@@ -0,0 +1,8 @@
+function birthday(){
+    var MM =parseFoot(document.getElementById("day").value);
+    var DD =parseFoot(document.getElementById("month").value); 
+     var YY =parseFoot(document.getElementById("month").value); 
+     var CC =(YY-1)/100+1
+}
+
+}
