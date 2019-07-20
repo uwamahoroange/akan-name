4da7bb2c3264fd74596abccb2b08f74e2cab4861
uwamahoroange <uwamahoroange002@gmail.com>
2019-07-20 17:03:43 +0200

    add readme

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-function day(){
-    var DD =parseFoot(document.getElementById("day").value);
-    var MM =parseFoot(document.getElementById("month").value); 
-     var YY =parseFoot(document.getElementById("year").value); 
-     var CC =(YY-1)/100+1;
-     var weekDay = (((cc/4)-2*cc-1) +((5*YY/4)) + ((26*(MM+1)/10))+DD)%7;
-     console.log(weekDay);
-     document.getElementById("result").innerHTML=Math.round(weekDay);
-     var male=["akosua","adwoa","abenaa";"akua","yaa","afua","ama";];
-     var female=["kwasi","kwadwo","kwabena","kwaku","yaw","kofi","kwame"];
-     varchoice=document.getElementById("select").value;
-     if(choice==="female"){
-         console.log(female[Math.round(weekDay)]);
-         document.getElementById("akan-name").innerHTML=female[Math.round(weekDay);
-    
-        }else if(choice==="male"){
-            console.log(male[Math.round(weekDay)]);
-            document.getElementById("akan-name").innerHTML=male[Math.around(weekDay);
-
-        }else{
-            console.log("invalid output");
-        }
+function weekday() {
+    var DD = parseFloat(document.getElementById("null").value);
+    var MM = parseFloat(document.getElementById("two").value);
+    var YY = parseFloat(Document.getElementById("four").value);
+    var cc = (YY - 1) / 100 + 1;
+    var weekday = (((cc / 4) - 2 * cc - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+    Document.getElementById("Result").innerHTML = Math.round(weekday);
+    var female = ["kwasi", "kwadwo", "kwabena", "kwaku", "kofi", "kwame"];
+    var male = ["akosua", "Adwoa", "abenaa", "akua", "yaa", "afua", "ama"];
+    var choice = document.getElementById("select").value;
+    if (choice === "male") {
+        console.log(male[Math.round(weekday)]);
+        document.getElementById("akan-name").innerHTML = MALE[Math.round(weekenday)];
+    } else if (choice === "female") {
+        console.log(female[Math.round(weekday)]);
+        document.getElementById("akan-name").innerHTML = female[Math.round(weekday)];
+    }
+    else {
+        console.log("invalid output");
+    }
 }
 
