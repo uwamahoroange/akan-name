93431b6892bf81d45f76914935d13012b33d1bc8
uwamahoroange <uwamahoroange002@gmail.com>
2019-07-20 12:58:00 +0200

    add var

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ function birthday(){
     var MM =parseFoot(document.getElementById("day").value);
     var DD =parseFoot(document.getElementById("month").value); 
      var YY =parseFoot(document.getElementById("month").value); 
-     var CC =(YY-1)/100+1
+     var CC =(YY-1)/100+1;
+     var weekendDay = (((cc/4)-2*cc-1) +((5*YY/4)) + ((26*(MM+1)/10))+DD)%7;
 }
 
 }
