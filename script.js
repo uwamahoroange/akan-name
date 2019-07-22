ec0c2971822c4e466154962ea96a8a131f99e816
uwamahoroange <uwamahoroange002@gmail.com>
2019-07-22 12:04:20 +0200

    modified my script

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 
-function weekday() {
-    var DD = parseFloat(document.getElementById("day")).value;
-    var MM = parseFloat(document.getElementById("month")).value;
-    var YY = parseFloat(document.getElementById("year")).value;
+function day() {
+    var DD = parseFloat(document.getElementById("day").value);
+    var MM = parseFloat(document.getElementById("month").value);
+    var YY = parseFloat(document.getElementById("year").value);
     var cc = (YY - 1) / 100 + 1;
-    var weekday = Math.round( ( (cc/4) -2*cc-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    var weekday =( ( (cc/4) -2*cc-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+    document.getElementById("result").innerHTML = Math.round(weekday);
 
-    var female = ["kwasi", "kwadwo", "kwabena", "kwaku", "kofi", "kwame"];
-    var male = ["akosua", "Adwoa", "abenaa", "akua", "yaa", "afua", "ama"];
+    var males = ["kwasi", "kwadwo", "kwabena", "kwaku", "kofi", "kwame"];
+    var females = ["akosua", "Adwoa", "abenaa", "akua", "yaa", "afua", "ama"];
+
     var choice = document.getElementById("select").value;
     if (choice === "male") {
+        //   {
+            console.log(males[Math.round(weekday)]);
+        document.getElementById("name").innerHTML = males[Math.round(weekday)];
+
+    }else if (choice === "female") {
         // if (weekday === 7) {
-        document.getElementById("result").innerHTML = male[weekday-1];
+            console.log(females[Math.round(weekday)]);
+        document.getElementById("name").innerHTML = females[Math.round(weekday)];
+    }else{
+           console.log("invalid");
+       }
     }
-    else if (choice === "female") {
-        // if (weekday === 7) {
-        document.getElementById("result").innerHTML = female[weekday-1];
-    }
-}
